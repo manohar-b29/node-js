f822414cc3c3443ed40f0e344d3c9e657465be75
root <root@ip-172-31-4-146.ap-south-1.compute.internal>
2025-08-23 19:37:25 +0000

    app.js modified

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
-console.log("Hello from Node.js! Jenkins pipeline is working.");
+// app.js
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.end('Hello from Node.js! Jenkins pipeline is working.');
+});
+
+server.listen(3000, () => {
+  console.log('Server running on port 3000');
+});
 
