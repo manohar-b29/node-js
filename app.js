2b2ae851650cd29e708fc210116438501c196331
manohar-b29 <manohar.barre@gmail.com>
2025-09-03 12:52:21 +0000

    app

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-// app.js
 const http = require('http');
+
+const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.end('Hello from Node.js! Jenkins pipeline is working.');
 });
 
-server.listen(3000, () => {
-  console.log('Server running on port 3000');
+server.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
 });
 
